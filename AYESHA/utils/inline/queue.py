33326edc2400fb4bf8f4457c547e31573a8f0ca2
JHYHDYHDUDJDJNEUBDUBTHEIOMWYI890 <theriyamusic94@gmail.com>
2025-01-06 17:40:14 +0530

    Update queue.py

--- AYESHA/utils/inline/queue.py
+++ AYESHA/utils/inline/queue.py
@@ -73,10 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="✭ ᴜᴘᴅᴀᴛᴇ ❥", url=f"https://t.me/RIYA_NETWORK"
+                text="✭ ᴜᴘᴅᴀᴛᴇ ❥", url=f"https://t.me/l_dil_ki_baat_l"
             ),
             InlineKeyboardButton(
-                text="✭ sᴜᴘᴘᴏʀᴛ ❥", url=f"https://t.me/RIYA_CHAT_SUPPORT"
+                text="✭ sᴜᴘᴘᴏʀᴛ ❥", url=f"https://t.me/l_apni_duniya_l"
             ),
         ],
       ]
